efbb69e513e9befd90786bcdf2fec89d00813f40
SayAmeen690 <129009869+Sheriff690@users.noreply.github.com>
2025-01-07 09:50:00 +0000

    Changed some lines of code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import dynamic from "next/dynamic";
 import Image from "next/image";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+// Dynamically import react-slick with SSR disabled
+const Slider = dynamic(() => import("react-slick"), { ssr: false });
 
 const images = [
   "/images/Wp2.jpg",
@@ -24,10 +29,19 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <p>Confirm the image exists in the public/images folder.
+        Confirm the development server is running (npm run dev).</p>
         <Slider {...settings} className="w-full">
           {images.map((src, index) => (
             <div key={index}>
-              <Image src={src} alt={`Image ${index + 1}`} width={180} height={38} priority className="w-full" />
+              <Image
+                src={src}
+                alt={`Image ${index + 1}`}
+                width={180}
+                height={38}
+                priority
+                className="w-full"
+              />
             </div>
           ))}
         </Slider>
